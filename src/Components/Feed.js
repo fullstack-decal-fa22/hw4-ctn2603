ed845033141454d405b404caa3e327fdbaef7f53
ctn2603 <cnguyen2603@berkeley.edu>
2022-10-18 07:14:54 -0700

    finished hw4

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -4,17 +4,13 @@ import Menu from './Menu';
 import Block from './Block';
 
 const Feed = () => {
-    /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
-    // Refer to Hint 2 for more help!
-
-    /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const [blocks, setBlocks] = useState([])
+    const posts = blocks.map((item, key) => <Block key={key} color={item}/>)
 
     return (
         <div>
-            <Menu></Menu>
+            <Menu createPost={(color) => setBlocks((prevState) => [...prevState, color])}/>
 
-            {/* Below is where all of your Blocks should render! */}
             {posts}
         </div>
     );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,22 @@
-import React from 'react';
-/* Add any imports you think you might need here! */
+import React from "react";
+import Color from './Color.js';
 
-const Menu = () => { 
-
-    return (
-      <div className="colorOptions">
-          {/* TODO */}
-      </div>
-    );
-}
+const Menu = (props) => {
+	return (
+		<div className="colorOptions">
+			{
+				["red", "pink", "blue", "green"].map((item, key) => {
+					return (
+						<Color
+							key={key}
+							color={item}
+							handleClick={() => props.createPost(item)}
+						/>
+					);
+				})
+			}
+		</div>
+	);
+};
 
 export default Menu;
